be8dae9d3410f1f59109bc9f89395c5a04637c7c
A.Rozovskaya <A.Rozovskaya@goodt.me>
2020-03-21 22:52:29 +0300

    typo

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 class CardNumberMaskerImpl: CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        require (start < end) { "Start index cannot be greater than end index" }
+        require (start <= end) { "Start index cannot be greater than end index" }
         if (cardNumber.isNotEmpty()) {
             val actualEnd = if (cardNumber.length < end) cardNumber.length else end
             return cardNumber.replaceRange(start, actualEnd, maskChar.toString().repeat(actualEnd - start))
